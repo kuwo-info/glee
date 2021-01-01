1697591b72c37c93fe8ed67ccb13ae760f15ccc5
1891021h <1891021h@stu.kobe-u.ac.jp>
2021-01-01 19:04:41 +0900

    inner file

--- 01_transport/ajax.js
+++ 01_transport/ajax.js
@@ -1,10 +1,21 @@
-$(function() {
-    $.ajax({
-        type: 'GET',
-        url: 'test.html',
-        dataType: 'html',
-        success: function(data) {
-            console.log($(data).find('#sample').text());
-        }
+var head = document.getElementsByTagName('head')
+var script = document.createElement('script')
+//ここで任意のjQueryを読み込ませる
+script.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js')
+script.setAttribute('type', 'text/javascript')
+script.addEventListener('load', function() {
+
+	// ここにjQueryの記述をする
+    document.head.appendChild(script)
+    $(function() {
+        $.ajax({
+            type: 'GET',
+            url: 'test.html',
+            dataType: 'html',
+            success: function(data) {
+                console.log($(data).find('#sample').text());
+            }
+        });
     });
-});
+
+})
